758ea2e51f40d2d55ec4e222253c947bf8c0c8cc
Louis Arge <louislva@gmail.com>
2020-04-01 16:19:16 +0200

    changed logo to navigate to "/" instead of landing page

--- app/src/components/nav/index.js
+++ app/src/components/nav/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import R from 'ramda';
-import Login from '../loginBtn';
-import Logout from '../logoutBtn';
-import MenuItem from './menuItem';
-import DropdownItem from './dropdownItem';
-import SearchBar from '../searchBar';
-import './style.css';
+import React, { Component } from "react";
+import R from "ramda";
+import Login from "../loginBtn";
+import Logout from "../logoutBtn";
+import MenuItem from "./menuItem";
+import DropdownItem from "./dropdownItem";
+import SearchBar from "../searchBar";
+import "./style.css";
 
 class Nav extends Component {
   constructor() {
@@ -16,7 +16,8 @@ class Nav extends Component {
   }
   render() {
     const user = this.props.user;
-    const candidate = user && R.find(R.propEq('id', user.id), this.props.candidateList);
+    const candidate =
+      user && R.find(R.propEq("id", user.id), this.props.candidateList);
     const toggleMenu = () => {
       this.setState({ showMenu: !this.state.showMenu });
     };
@@ -24,8 +25,12 @@ class Nav extends Component {
       <nav className="fixed pin-t pin-x z-10 bg-white border-b border-grey-lighter shadow select-none p-2">
         <div className="flex max-w-xl mx-auto relative">
           <div className="absolute pin-y flex items-center -ml-12">
-            <a href="https://lykkepartiet.dk/" target="_lykkepartiet" rel="noopener noreferrer">
-              <img src="/favicon.png" alt="Lykkepartiets logo" className="block w-6 h-6" />
+            <a href="/" rel="noopener noreferrer">
+              <img
+                src="/favicon.png"
+                alt="Lykkepartiets logo"
+                className="block w-6 h-6"
+              />
             </a>
           </div>
           <div className="hidden md:flex flex-grow flex-no-shrink pr-2">
@@ -33,10 +38,13 @@ class Nav extends Component {
               <MenuItem to="/proposals" itemName="Forslag" />
               <MenuItem to="/projects" itemName="Projekter" />
               <MenuItem to="/candidates" itemName="Kandidater" />
-              {user &&
-                user.firstname && (
-                  <MenuItem to="/insights" itemName="Historik" className="md:border-l md:border-grey-lightest pl-2" />
-                )}
+              {user && user.firstname && (
+                <MenuItem
+                  to="/insights"
+                  itemName="Historik"
+                  className="md:border-l md:border-grey-lightest pl-2"
+                />
+              )}
             </ul>
           </div>
           <SearchBar
@@ -50,42 +58,69 @@ class Nav extends Component {
               <div>
                 <button
                   className={
-                    (this.state.showMenu ? 'text-white bg-grey-darkest' : 'bg-grey-lightest') +
-                    ' inline-block leading-tight font-bold rounded-sm relative z-30 p-2'
+                    (this.state.showMenu
+                      ? "text-white bg-grey-darkest"
+                      : "bg-grey-lightest") +
+                    " inline-block leading-tight font-bold rounded-sm relative z-30 p-2"
                   }
                   onClick={toggleMenu}
-                  onMouseDown={e => e.preventDefault()}>
+                  onMouseDown={e => e.preventDefault()}
+                >
                   {user.firstname.charAt(0) + user.lastname.charAt(0)}
                 </button>
                 <div
                   className={
-                    (!this.state.showMenu && 'hidden ') +
-                    ' absolute pin-t pin-r w-screen md:w-auto min-w-48 pl-4 pt-2 mt-9 menu-animation'
-                  }>
+                    (!this.state.showMenu && "hidden ") +
+                    " absolute pin-t pin-r w-screen md:w-auto min-w-48 pl-4 pt-2 mt-9 menu-animation"
+                  }
+                >
                   <div className="text-white bg-grey-darkest rounded-sm relative z-30">
                     <span className="block border-b border-grey-darker p-4">
-                      {user.firstname + ' ' + user.lastname}
+                      {user.firstname + " " + user.lastname}
                     </span>
                     <ul className="md:hidden list-reset border-b border-grey-darker p-2">
-                      <DropdownItem to="/proposals" itemName="Forslag" onClick={toggleMenu} />
-                      <DropdownItem to="/projects" itemName="Projekter" onClick={toggleMenu} />
-                      <DropdownItem to="/candidates" itemName="Kandidater" onClick={toggleMenu} />
-                      {user &&
-                        user.firstname && (
-                          <DropdownItem
-                            to="/insights"
-                            itemName="Historik"
-                            className="md:border-l md:border-grey-lightest pl-2"
-                            onClick={toggleMenu}
-                          />
-                        )}
+                      <DropdownItem
+                        to="/proposals"
+                        itemName="Forslag"
+                        onClick={toggleMenu}
+                      />
+                      <DropdownItem
+                        to="/projects"
+                        itemName="Projekter"
+                        onClick={toggleMenu}
+                      />
+                      <DropdownItem
+                        to="/candidates"
+                        itemName="Kandidater"
+                        onClick={toggleMenu}
+                      />
+                      {user && user.firstname && (
+                        <DropdownItem
+                          to="/insights"
+                          itemName="Historik"
+                          className="md:border-l md:border-grey-lightest pl-2"
+                          onClick={toggleMenu}
+                        />
+                      )}
                     </ul>
                     <ul className="list-reset border-b border-grey-darker p-2">
-                      <DropdownItem to="/projects/mine" itemName="Mine projekter" onClick={toggleMenu} />
+                      <DropdownItem
+                        to="/projects/mine"
+                        itemName="Mine projekter"
+                        onClick={toggleMenu}
+                      />
                       {candidate && (
-                        <DropdownItem to={`/candidate/${user.id}`} itemName="Mit kandidatur" onClick={toggleMenu} />
+                        <DropdownItem
+                          to={`/candidate/${user.id}`}
+                          itemName="Mit kandidatur"
+                          onClick={toggleMenu}
+                        />
                       )}
-                      <DropdownItem to="/preferences" itemName="Mine præferencer" onClick={toggleMenu} />
+                      <DropdownItem
+                        to="/preferences"
+                        itemName="Mine præferencer"
+                        onClick={toggleMenu}
+                      />
                       {/* <DropdownItem to="/settings" itemName="Mine indstillinger" onClick={toggleMenu} /> */}
                     </ul>
                     <div className="p-2">
@@ -103,23 +138,39 @@ class Nav extends Component {
                 <div className="md:hidden">
                   <button
                     className={
-                      (this.state.showMenu ? 'text-white bg-grey-darkest' : 'bg-grey-lightest') +
-                      ' inline-block leading-tight font-bold rounded-sm relative z-30 p-2'
+                      (this.state.showMenu
+                        ? "text-white bg-grey-darkest"
+                        : "bg-grey-lightest") +
+                      " inline-block leading-tight font-bold rounded-sm relative z-30 p-2"
                     }
                     onClick={toggleMenu}
-                    onMouseDown={e => e.preventDefault()}>
+                    onMouseDown={e => e.preventDefault()}
+                  >
                     Menu
                   </button>
                   <div
                     className={
-                      (!this.state.showMenu && 'hidden ') +
-                      ' absolute pin-t pin-r w-screen md:w-auto min-w-48 pl-4 pt-2 mt-9 menu-animation'
-                    }>
+                      (!this.state.showMenu && "hidden ") +
+                      " absolute pin-t pin-r w-screen md:w-auto min-w-48 pl-4 pt-2 mt-9 menu-animation"
+                    }
+                  >
                     <div className="text-white bg-grey-darkest rounded-sm relative z-30">
                       <ul className="md:hidden list-reset border-b border-grey-darker p-2">
-                        <DropdownItem to="/proposals" itemName="Forslag" onClick={toggleMenu} />
-                        <DropdownItem to="/projects" itemName="Projekter" onClick={toggleMenu} />
-                        <DropdownItem to="/candidates" itemName="Kandidater" onClick={toggleMenu} />
+                        <DropdownItem
+                          to="/proposals"
+                          itemName="Forslag"
+                          onClick={toggleMenu}
+                        />
+                        <DropdownItem
+                          to="/projects"
+                          itemName="Projekter"
+                          onClick={toggleMenu}
+                        />
+                        <DropdownItem
+                          to="/candidates"
+                          itemName="Kandidater"
+                          onClick={toggleMenu}
+                        />
                       </ul>
                       <div className="p-2">
                         <Login
